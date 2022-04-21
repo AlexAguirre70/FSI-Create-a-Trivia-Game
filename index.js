7f9a65b04061c028748f738f7c7f7b9e92946bd8
Alex Aguirre <teamaguirre@gmail.com>
2022-04-21 13:23:25 -0700

    added the JS script to create the trivia game

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your Code Here
+let userName = window.prompt ("Please enter your Name")
+let userScore = 0
+for(let i=0; i<questions.length; i++){
+    let userAnswer = window.prompt(questions[i].text)
+    if (userAnswer === questions[i].correctAnswer)   
+     {
+         userScore= userScore + 10
+     }
+}
+window.alert("Your Score is : "+ userScore)
